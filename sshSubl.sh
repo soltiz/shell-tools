814491c483f2055864b0d1a2b38cd0d052ef9561
Cedric VAN FRACHEM <cedric.vanfrachem@thalesgroup.com>
2014-06-05 14:48:14 +0200

    initial commit with only sshSubl.sh remote editing ugly wrapper for sublime text

---
+++ sshSubl.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -u
+
+
+bindir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+lmcLibDir=${bindir}/../../lmc-dev/lmc-admin/bin
+
+
+. ${lmcLibDir}/lmc-lib.sh
+
+# remote file should be with RCP syntax : [user]@host:path
+remoteFile=$1
+remoteHost=${remoteFile%%:*}
+
+
+waitForSshSuccess ${remoteHost} || fatal "Cannot remote edit"
+
+tmpdir=$(mktemp -d ${remoteHost}-XXXXXX )
+lastpart=$(basename ${remoteFile##*:})
+localFile=${tmpdir}/${lastpart}
+
+
+log "Starting remote edit on file '$remoteFile'..."
+
+rcp "${remoteFile}" "${localFile}" || (touch "${localFile}" && warn "This is a new file")
+
+subl "${localFile}"
+
+trap ctrl_c INT 
+function ctrl_c() {
+	log "End of remote edition for file '${remoteFile}'."
+	rm -rf "${tmpdir}"
+	exit 0
+}
+
+while true; do
+	 inotifywait -e close_write,moved_to,create "${localFile}" > /dev/null
+	 log -n "File '${lastpart}' has changed. Updating '${remoteFile}'..."
+	 rcp "${localFile}" "${remoteFile}" || error "COULD NOT UPDATE REMOTE FILE '${remoteFile}'."
+	 log " UPDATED at $(date)."
+done
+
+
